7e7572aaae2cc89f1367b1c0edddd72a6ec00602
Ali Khan <alik95997@gmail.com>
2026-02-01 14:34:32 +0500

    moved components folder into src

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "../components/Header/Header";
-import HeroSection from "../components/Hero-Section/HeroSection";
-import AboutSection from "../components/AboutSection/AboutSection";
-import CultureSection from "../components/CultureSection/CultureSection";
-import Stats from "../components/Stats/Stats";
-import Vacancy from "../components/Vacancy/Vacancy";
-import PostSection from "../components/PostSection/PostSection";
-import Footer from "../components/Footer/Footer";
+import Header from "./components/Header/Header";
+import HeroSection from "./components/Hero-Section/HeroSection";
+import AboutSection from "./components/AboutSection/AboutSection";
+import CultureSection from "./components/CultureSection/CultureSection";
+import Stats from "./components/Stats/Stats";
+import Vacancy from "./components/Vacancy/Vacancy";
+import PostSection from "./components/PostSection/PostSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
