df56ed57331b7637cb9e85a491d8d0083f9b1f70
Ali Khan <alik95997@gmail.com>
2026-02-01 16:41:13 +0500

    added position sticky in header.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,39 +3,40 @@ import "./header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FoodpandaLogo from "../../assets/logo.svg";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import FindMoreButton from "../FindMoreButton/FindMoreButton";
 const Header = () => {
   return (
     <header>
-      <div>
-        <img src={FoodpandaLogo} alt="" />
-      </div>
+      <div className="headerWrapper">
+        <div className="header-logo-container">
+          <img src={FoodpandaLogo} alt="" />
+        </div>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="">About Us </a>
-          </li>
-          <li>
-            <a href="">Why Foodpanda</a>
-          </li>
-          <li>
-            <a href="">Jobs</a>
-          </li>
-          <li>
-            <a href="">Locations</a>
-          </li>
-          <li>
-            <a href="">pandaStories</a>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <a href="">About Us </a>
+            </li>
+            <li>
+              <a href="">Why Foodpanda</a>
+            </li>
+            <li>
+              <a href="">Jobs</a>
+            </li>
+            <li>
+              <a href="">Locations</a>
+            </li>
+            <li>
+              <a href="">pandaStories</a>
+            </li>
+          </ul>
+        </nav>
 
-      <div>
-        <span className="saved-job">
-          Saved Jobs
-          <FontAwesomeIcon icon={faStar} />
-        </span>
+        <div>
+          <span className="saved-job">
+            Saved Jobs
+            <FontAwesomeIcon icon={faStar} />
+          </span>
+        </div>
       </div>
     </header>
   );
--- src/components/Hero-Section/HeroSection.jsx
+++ src/components/Hero-Section/HeroSection.jsx
@@ -6,26 +6,27 @@ import {
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 
-
 const HeroSection = () => {
   return (
     <section className="hero">
-      <div className="hero-left">
-        <div className="hero-text">
-          <h1>Hungry to make an impact</h1>
-          <h2>There’s always room at our table.</h2>
-        </div>
-        <div className="search-wrapper">
-          <input type="text" placeholder="Start your job search here" />
+      <div className="heroWrapper">
+        <div className="hero-left">
+          <div className="hero-text">
+            <h1>Hungry to make an impact</h1>
+            <h2>There’s always room at our table.</h2>
+          </div>
+          <div className="search-wrapper">
+            <input type="text" placeholder="Start your job search here" />
 
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faMagnifyingGlass}className="searchWrapperIcon" />
+          </div>
         </div>
-      </div>
-      <div className="hero-img-container">
-        <img
-          src="https://attraxcdnprod1-freshed3dgayb7c3.z01.azurefd.net/1481181/0f1cd41f-b5a4-4fa7-b963-a5706b26c90b/0.0.0/img/2Nn1Y5_G9EZl9Qjb1WHOsg/"
-          alt=""
-        />
+        <div className="hero-img-container">
+          <img
+            src="https://attraxcdnprod1-freshed3dgayb7c3.z01.azurefd.net/1481181/0f1cd41f-b5a4-4fa7-b963-a5706b26c90b/0.0.0/img/2Nn1Y5_G9EZl9Qjb1WHOsg/"
+            alt=""
+          />
+        </div>
       </div>
     </section>
   );
